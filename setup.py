46229e52ef5129ca36fbb0c45a434a84958ee005
Torsten Sommer <torsten.sommer@modelon.com>
2015-09-24 15:55:14 +0200

    setup.py and license added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name = "fmpy",
+      version = "0.0.1",
+      description = "Simulate Functional Mockup Units (FMUs) in Python",
+      author = "Torsten Sommer",
+      author_email = "torsten.sommer@3ds.com",
+      url = "https://bitbucket.org/3DS/FMpy",
+      license = "Standard 3-clause BSD",
+      packages = ['fmpy',],
+      #long_description = """Simulate Functional Mockup Units (FMUs) in Python.""" ,
+)

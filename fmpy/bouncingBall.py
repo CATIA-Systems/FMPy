310cca75df98e13801ef82311f35db21f948c6ed
Torsten Sommer <torsten.sommer@modelon.com>
2015-09-24 15:58:32 +0200

    Example fixed.

--- fmpy/bouncingBall.py
+++ fmpy/bouncingBall.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from fmpy.simulate import simulate
 
 
-result = simulate(filename=r'Z:\Development\FMPy\bouncingBall.fmu', start_values={'h': 1.5})
+result = simulate(filename='bouncingBall.fmu', start_values={'h': 1.5})
 
 plt.plot(result['time'], result['h'])
 plt.plot(result['time'], result['der(h)'])

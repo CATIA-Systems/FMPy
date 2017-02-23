aede77c78dd9aaba66827a8c9d1d58166b9a1eb2
Torsten Sommer <torsten.sommer@3ds.com>
2017-02-23 22:03:05 +0100

    Recorder added.

--- fmpy/simulate1.py
+++ fmpy/simulate1.py
@@ -9,9 +9,72 @@ import zipfile
 from .modelDescription import read_model_description
 
 
+class Recorder(object):
+
+    def __init__(self, fmu, output=None):
+
+        self.fmu = fmu
+
+        md = fmu.modelDescription
+
+        self.cols = [('time', np.float64)]
+        self.rows = []
+
+        self.values = {'Real': [], 'Integer': [], 'Boolean': [], 'String': []}
+
+        for sv in md.modelVariables:
+            if (output is None and sv.causality == 'output') or (output is not None and sv.name in output):
+                self.values[sv.type].append((sv.name, sv.valueReference))
+
+        if len(self.values['Real']) > 0:
+            real_names, real_vrs = zip(*self.values['Real'])
+            self.real_vrs = (fmi1ValueReference * len(real_vrs))(*real_vrs)
+            self.real_values = (fmi1Real * len(real_vrs))()
+            self.cols += zip(real_names, [np.float64] * len(real_names))
+        else:
+            self.real_vrs = []
+
+        if len(self.values['Integer']) > 0:
+            integer_names, integer_vrs = zip(*self.values['Integer'])
+            self.integer_vrs = (fmi1ValueReference * len(integer_vrs))(*integer_vrs)
+            self.integer_values = (fmi1Integer * len(integer_vrs))()
+            self.cols += zip(integer_names, [np.int32] * len(integer_names))
+        else:
+            self.integer_vrs = []
+
+        if len(self.values['Boolean']) > 0:
+            boolean_names, boolean_vrs = zip(*self.values['Boolean'])
+            self. boolean_vrs = (fmi1ValueReference * len(boolean_vrs))(*boolean_vrs)
+            self.boolean_values = (fmi1Boolean * len(boolean_vrs))()
+            self.cols += zip(boolean_names, [np.int32] * len(boolean_names))
+        else:
+            self.boolean_vrs = []
+
+    def sample(self, time):
+
+        row = [time]
+
+        if len(self.real_vrs) > 0:
+            status = self.fmu.fmi1GetReal(self.fmu.component, self.real_vrs, len(self.real_vrs), self.real_values)
+            row += list(self.real_values)
+
+        if len(self.integer_vrs) > 0:
+            status = self.fmu.fmi1GetInteger(self.fmu.component, self.integer_vrs, len(self.integer_vrs), self.integer_values)
+            row += list(self.integer_values)
+
+        if len(self.boolean_vrs) > 0:
+            status = self.fmu.fmi1GetBoolean(self.fmu.component, self.boolean_vrs, len(self.boolean_vrs), self.boolean_values)
+            row += list(self.boolean_values)
+
+        self.rows.append(tuple(row))
+
+    def result(self):
+        return np.array(self.rows, dtype=np.dtype(self.cols))
+
+
 def simulate(filename, start_time=0, stop_time=1, step_size=1e-3, start_values={}, output=None):
 
-    md = read_model_description(filename)
+    modelDescription = read_model_description(filename)
 
     time = start_time
 
@@ -25,66 +88,17 @@ def simulate(filename, start_time=0, stop_time=1, step_size=1e-3, start_values={
     with zipfile.ZipFile(filename, 'r') as fmufile:
         fmufile.extractall(unzipdir)
 
-    if md.fmiVersion == '1.0':
-        fmu = FMU1Slave(modelDescription=md, unzipDirectory=unzipdir, instanceName="instance1")
+    if modelDescription.fmiVersion == '1.0':
+        fmu = FMU1Slave(modelDescription=modelDescription, unzipDirectory=unzipdir, instanceName="instance1")
         fmu.instantiate("rectifier1")
         fmu.initializeSlave()
         pass
 
-    cols = [('time', np.float64)]
-    result = []
-
-    values = {'Real': [], 'Integer': [], 'Boolean': [], 'String': []}
-
-    for sv in md.modelVariables:
-        if (output is None and sv.causality == 'output') or (output is not None and sv.name in output):
-            values[sv.type].append((sv.name, sv.valueReference))
-
-    if len(values['Real']) > 0:
-        real_names, real_vrs = zip(*values['Real'])
-        real_vrs = (fmi1ValueReference * len(real_vrs))(*real_vrs)
-        real_values = (fmi1Real * len(real_vrs))()
-        cols += zip(real_names, [np.float64] * len(real_names))
-    else:
-        real_vrs = []
-
-    if len(values['Integer']) > 0:
-        integer_names, integer_vrs = zip(*values['Integer'])
-        integer_vrs = (fmi1ValueReference * len(integer_vrs))(*integer_vrs)
-        integer_values = (fmi1Integer * len(integer_vrs))()
-        cols += zip(integer_names, [np.int32] * len(integer_names))
-    else:
-        integer_vrs = []
-
-    if len(values['Boolean']) > 0:
-        boolean_names, boolean_vrs = zip(*values['Boolean'])
-        boolean_vrs = (fmi1ValueReference * len(boolean_vrs))(*boolean_vrs)
-        boolean_values = (fmi1Boolean * len(boolean_vrs))()
-        cols += zip(boolean_names, [np.int32] * len(boolean_names))
-    else:
-        boolean_vrs = []
-
+    recorder = Recorder(fmu=fmu, output=output)
 
     while time < stop_time:
-
-        row = [time]
-
-        if len(real_vrs) > 0:
-            status = fmu.fmi1GetReal(fmu.component, real_vrs, len(real_vrs), real_values)
-            row += list(real_values)
-
-        if len(integer_vrs) > 0:
-            status = fmu.fmi1GetInteger(fmu.component, integer_vrs, len(integer_vrs), integer_values)
-            row += list(integer_values)
-
-        if len(boolean_vrs) > 0:
-            status = fmu.fmi1GetBoolean(fmu.component, boolean_vrs, len(boolean_vrs), boolean_values)
-            row += list(boolean_values)
-
-        result.append(tuple(row))
-
+        recorder.sample(time)
         status = fmu.doStep(currentCommunicationPoint=time, communicationStepSize=step_size)
-
         time += step_size
 
     # clean up
@@ -92,5 +106,4 @@ def simulate(filename, start_time=0, stop_time=1, step_size=1e-3, start_values={
     fmu.freeSlaveInstance()
     shutil.rmtree(unzipdir)
 
-    # return the result
-    return np.array(result, dtype=np.dtype(cols))
+    return recorder.result()

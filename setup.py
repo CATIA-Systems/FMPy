1e47507f2608f1a3d302a2b1eeb8cdccf885b493
Torsten Sommer <torsten.sommer@3ds.com>
2017-05-21 22:15:24 +0200

    Version pulled up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name = "fmpy",
-      version = "0.0.1",
+      version = "0.0.2",
       description = "Simulate Functional Mockup Units (FMUs) in Python",
       author = "Torsten Sommer",
       author_email = "torsten.sommer@3ds.com",

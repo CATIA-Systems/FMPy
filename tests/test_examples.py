579d32d5881099deb085bfe5ee3761b273fc433d
Torsten Sommer <torsten.sommer@3ds.com>
2017-05-22 15:22:50 +0200

    Test renamed.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,11 +2,13 @@ import unittest
 from fmpy.examples.coupled_clutches import simulate_coupled_clutches
 
 
-class TestFMUInfo(unittest.TestCase):
+class ExamplesTest(unittest.TestCase):
 
     def test_coupled_clutches_example(self):
 
-        simulate_coupled_clutches(False)
+        simulate_coupled_clutches(show_plot=False)
+
+        # TODO: add assertions
 
 
 if __name__ == '__main__':

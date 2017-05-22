6ad8c63964f9a420b63f1a91c7eea7a35dfccda5
Torsten Sommer <torsten.sommer@3ds.com>
2017-05-22 16:36:39 +0200

    Test fixed.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,12 +1,20 @@
 import unittest
 from fmpy.examples.coupled_clutches import simulate_coupled_clutches
-from fmpy import CO_SIMULATION, MODEL_EXCHANGE
+from fmpy import CO_SIMULATION, MODEL_EXCHANGE, platform
 
 class ExamplesTest(unittest.TestCase):
 
     def test_coupled_clutches_example(self):
 
-        for fmi_version in ['1.0', '2.0']:
+
+        if platform.startswith('win'):
+            fmi_versions = ['1.0', '2.0']
+        elif platform.startswith('linux'):
+            fmi_versions = ['2.0']
+        else:
+            self.fail('Platform not supported')
+
+        for fmi_version in fmi_versions:
             for fmi_type in [CO_SIMULATION, MODEL_EXCHANGE]:
                 simulate_coupled_clutches(fmi_version=fmi_version, fmi_type=fmi_type, show_plot=False)
 

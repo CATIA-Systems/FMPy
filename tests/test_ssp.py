7d133b2938dedf0563386a97119bf9bc8f9e0447
Torsten Sommer <torsten.sommer@3ds.com>
2017-12-20 16:15:57 +0100

    Update SSP schema to Draft20171219.

--- tests/test_ssp.py
+++ tests/test_ssp.py
@@ -2,9 +2,9 @@ import unittest
 from unittest import skipIf
 import numpy as np
 import sys
+from fmpy import platform
 from fmpy.ssp.ssd import read_ssd, read_ssv
 from fmpy.ssp.simulation import simulate_ssp
-from fmpy.util import plot_result
 import os
 
 
@@ -16,9 +16,11 @@ class SSPTest(unittest.TestCase):
         print("Python:", sys.version)
         print()
 
-    def ssp_example_path(self, filename):
+    @staticmethod
+    def ssp_example_path(filename):
         return os.path.join(os.environ['SSP_EXAMPLES_DIR'], filename)
 
+    @skipIf(platform != 'win32', "Current platform not supported by this SSP")
     def test_simulate_sample_system_with_parameters(self):
 
         ssv_filename = self.ssp_example_path('SampleSystemParameterValues.ssv')
@@ -33,6 +35,7 @@ class SSPTest(unittest.TestCase):
 
         # plot_result(result, names=['In1', 'Out1'], window_title=filename)
 
+    @skipIf(platform != 'win32', "Current platform not supported by this SSP")
     def test_simulate_sub_system(self):
 
         ssp_filename = self.ssp_example_path('SampleSystemSubSystem.ssp')
@@ -44,39 +47,27 @@ class SSPTest(unittest.TestCase):
 
         # plot_result(result, names=['In1', 'Out1'], window_title=ssp_filename)
 
-    # def test_multi_array(self):
-    #
-    #     from fmpy.sundials import N_VNew_Serial, NV_DATA_S
-    #
-    #     x = N_VNew_Serial(4)
-    #     px = NV_DATA_S(x)
-    #
-    #     a1 = np.ctypeslib.as_array(px, (4,))
-    #
-    #     a1[0:2] = [1, 2]
-    #     a1[2:4] = [3, 4]
-
     def test_read_ssd_dictionary(self):
         filename = self.ssp_example_path('SampleSystemSubSystemDictionary.ssp')
         ssd = read_ssd(filename)
         self.assertEqual(ssd.name, 'Simple System with SubSystem and Dictionary')
 
-    def test_ControlledTemperature(self):
-
-        ssp_filename = r'ControlledTemperature.ssp'
-
-        if not os.path.isfile(ssp_filename):
-            return
-
-        def Tenv(t):
-            return 20.0
-
-        def Tref(t):
-            return 20.0 + np.floor(t) * 3
-
-        result = simulate_ssp(ssp_filename, stop_time=10, step_size=1e-2, input={'Tenv': Tenv, 'Tref': Tref})
-
-        # plot_result(result, names=['Tenv', 'Tref', 'controller.onSwitch', 'T'], window_title=os.path.basename(ssp_filename))
+    # def test_ControlledTemperature(self):
+    #
+    #     ssp_filename = r'ControlledTemperature.ssp'
+    #
+    #     if not os.path.isfile(ssp_filename):
+    #         return
+    #
+    #     def Tenv(t):
+    #         return 20.0
+    #
+    #     def Tref(t):
+    #         return 20.0 + np.floor(t) * 3
+    #
+    #     result = simulate_ssp(ssp_filename, stop_time=10, step_size=1e-2, input={'Tenv': Tenv, 'Tref': Tref})
+    #
+    #     # plot_result(result, names=['Tenv', 'Tref', 'controller.onSwitch', 'T'], window_title=os.path.basename(ssp_filename))
 
     # def test_eDrive(self):
     #     ssp_filename = r'Z:\Development\SSP\trunk\Examples\Electrical_Drive\Example_eDrive.ssp'

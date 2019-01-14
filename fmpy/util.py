925fc86a4710faef54d961d6d4ef18c50fe7719f
Torsten Sommer <wakemaniac@gmail.com>
2019-01-14 15:49:31 +0100

    Don't connect continuous segments in plot and use integral values in y-axis

--- fmpy/util.py
+++ fmpy/util.py
@@ -213,6 +213,7 @@ def plot_result(result, reference=None, names=None, filename=None, window_title=
     import matplotlib.pylab as pylab
     import matplotlib.pyplot as plt
     import matplotlib.transforms as mtransforms
+    from matplotlib.ticker import MaxNLocator
     from collections import Iterable
 
     params = {
@@ -274,10 +275,16 @@ def plot_result(result, reference=None, names=None, filename=None, window_title=
                 ax.fill_between(time, 0, 1, where=i_out, facecolor='red', alpha=0.5, transform=trans)
 
             if y.dtype == np.float64:
-                ax.plot(time, y, color='b', linewidth=0.9, label='result', zorder=101)
+                # find left indices of discontinuities
+                i_disc = np.flatnonzero(np.diff(time) == 0)
+                i_disc = np.append(i_disc + 1, len(time))
+                i0 = 0
+                for i1 in i_disc:
+                    ax.plot(time[i0:i1], y[i0:i1], color='b', linewidth=0.9, label='result', zorder=101)
+                    i0 = i1
             else:
-                ax.hlines(y, time[:-1], time[1:], colors='b', linewidth=1, label='result', zorder=101)
-                # ax.step(time, y, where='post', color='b', linewidth=0.9, label='result', zorder=101)
+                ax.hlines(y[:-1], time[:-1], time[1:], colors='b', linewidth=1, label='result', zorder=101)
+                ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
             if y.dtype == bool:
                 # use fixed range and labels and fill area

9ccfdcee68da3d1353829f583cd94ba274da41bb
Torsten Sommer <torsten.sommer@3ds.com>
2019-12-12 21:04:04 +0100

    Use "Apostrophe" in lark grammar
    instead of "Right Single Quotation Mark"

--- fmpy/model_description.py
+++ fmpy/model_description.py
@@ -757,11 +757,11 @@ def read_model_description(filename, validate=True, validate_variable_names=Fals
                 bname           : nondigit (nondigit|digit)* | qname
                 nondigit        : "_" | "a".."z" | "A".."Z"
                 digit           : "0".."9"
-                qname           : """ + u'"\u0027"' + r""" ( qchar | escape ) ( qchar | escape ) """ + u'"\u0027"' + r"""
+                qname           : "'" ( qchar | escape ) ( qchar | escape ) "'"
                 qchar           : nondigit | digit | "!" | "#" | "$" | "%" | "&" | "(" | ")" 
                                   | "*" | "+" | "," | "-" | "." | "/" | ":" | ";" | "<" | ">"
                                   | "=" | "?" | "@" | "[" | "]" | "^" | "{" | "}" | "|" | "~" | " "
-                escape          : """ + u'"\u0027"' + r""" | "\"" | "\?" | "\\" | "\a" | "\b" | "\f" | "\n" | "\r" | "\t" | "\v"
+                escape          : "\'" | "\"" | "\?" | "\\" | "\a" | "\b" | "\f" | "\n" | "\r" | "\t" | "\v"
                 arrayindices    : "[" unsignedinteger ("," unsignedinteger)* "]"
                 unsignedinteger : digit+
                 """

909576311a3af38741fb8829c7b3f6deefe2b082
Torsten Sommer <torsten.sommer@3ds.com>
2019-12-18 00:45:29 +0100

    Bump version to 0.2.15

--- fmpy/__init__.py
+++ fmpy/__init__.py
@@ -5,7 +5,7 @@ import os
 from ctypes import *
 import _ctypes
 
-__version__ = '0.2.14'
+__version__ = '0.2.15'
 
 
 # determine the platform
--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ extras_require = {
 extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
 
 setup(name='FMPy',
-      version='0.2.14',
+      version='0.2.15',
       description="Simulate Functional Mock-up Units (FMUs) in Python",
       long_description=long_description,
       author="Torsten Sommer",

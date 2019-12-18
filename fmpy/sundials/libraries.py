80f892af2808dcc0ba20be5382acf9fed8db75cd
Torsten Sommer <torsten.sommer@3ds.com>
2019-12-19 00:42:09 +0100

    Add pre-compiled SUNDIALS shared libraries

--- fmpy/sundials/libraries.py
+++ fmpy/sundials/libraries.py
@@ -1,11 +1,11 @@
 from ctypes import cdll
 import os
-from fmpy import sharedLibraryExtension
+from fmpy import sharedLibraryExtension, platform_tuple
 
 library_dir, _ = os.path.split(__file__)
 
 # load SUNDIALS shared libraries
-sundials_nvecserial     = cdll.LoadLibrary(os.path.join(library_dir, 'sundials_nvecserial'     + sharedLibraryExtension))
-sundials_sunmatrixdense = cdll.LoadLibrary(os.path.join(library_dir, 'sundials_sunmatrixdense' + sharedLibraryExtension))
-sundials_sunlinsoldense = cdll.LoadLibrary(os.path.join(library_dir, 'sundials_sunlinsoldense' + sharedLibraryExtension))
-sundials_cvode          = cdll.LoadLibrary(os.path.join(library_dir, 'sundials_cvode'          + sharedLibraryExtension))
+sundials_nvecserial     = cdll.LoadLibrary(os.path.join(library_dir, platform_tuple, 'sundials_nvecserial'     + sharedLibraryExtension))
+sundials_sunmatrixdense = cdll.LoadLibrary(os.path.join(library_dir, platform_tuple, 'sundials_sunmatrixdense' + sharedLibraryExtension))
+sundials_sunlinsoldense = cdll.LoadLibrary(os.path.join(library_dir, platform_tuple, 'sundials_sunlinsoldense' + sharedLibraryExtension))
+sundials_cvode          = cdll.LoadLibrary(os.path.join(library_dir, platform_tuple, 'sundials_cvode'          + sharedLibraryExtension))

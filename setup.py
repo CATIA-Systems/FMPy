ce3ed80b106326b2cb2fb0fa4e1fc1a287fc0d20
Torsten Sommer <torsten.sommer@3ds.com>
2022-04-27 20:57:18 +0200

    Bump version to 0.3.8

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ extras_require = {
 extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
 
 setup(name='FMPy',
-      version='0.3.7',
+      version='0.3.8',
       description="Simulate Functional Mock-up Units (FMUs) in Python",
       long_description=long_description,
       author="Torsten Sommer",

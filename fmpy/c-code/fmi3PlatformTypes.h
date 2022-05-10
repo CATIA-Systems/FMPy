d9547792af143b9e25f23073cb44a327289e0f5e
Torsten Sommer <torsten.sommer@3ds.com>
2022-05-10 21:27:25 +0200

    Update FMI API to v3.0

--- fmpy/c-code/fmi3PlatformTypes.h
+++ fmpy/c-code/fmi3PlatformTypes.h
@@ -2,7 +2,7 @@
 #define fmi3PlatformTypes_h
 
 /*
-This header file defines the data types of FMI 3.0-rc.2.
+This header file defines the data types of FMI 3.0.
 It must be used by both FMU and FMI master.
 
 Copyright (C) 2008-2011 MODELISAR consortium,

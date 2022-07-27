933d2fbe64b19533bcb7b36d79cdc9441c3faa88
Torsten Sommer <torsten.sommer@3ds.com>
2022-07-28 01:28:19 +0200

    Import FMUs in test

--- tests/run_ompython.py
+++ tests/run_ompython.py
@@ -1,3 +1,21 @@
-def test_ompython():
+import os
 
-    pass
+import OMPython
+from OMPython import OMCSessionZMQ
+
+os.environ['OPENMODELICAHOME'] = r'E:\OpenModelica'
+
+omc = OMCSessionZMQ()
+
+commands = [
+    'loadFile("E:/Development/FMPy/fmpy/modelica/FMI/package.mo")',
+    'loadFile("E:/Development/FMPy/tests/resources/FMITest/package.mo")',
+    'simulate(FMITest.BouncingBall_CS, stopTime=4)',
+]
+
+for command in commands:
+    print(command)
+    result = omc.sendExpression(command)
+    print(result)
+
+print('done')

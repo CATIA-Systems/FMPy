ebdc3dcab6fb17d1d0fc0143ca1feda5b60dd1af
Torsten Sommer <torsten.sommer@3ds.com>
2022-08-05 15:36:36 +0200

    Fix test

--- tests/test_import_to_modelica.py
+++ tests/test_import_to_modelica.py
@@ -1,46 +1,18 @@
-from pathlib import Path
-from shutil import move
-
+import numpy as np
 import pytest
 
+from fmpy import simulate_fmu, read_model_description
 from fmpy.modelica import import_fmu_to_modelica
+from fmpy.util import validate_signal
 
 
-# def pymola_available():
-#     try:
-#         import pymola
-#         return True
-#     except:
-#         return False
-#
-#
-# @pytest.mark.skipif(not pymola_available(), reason="Pymola was not found")
-# def test_import_fmu_to_modelica(dymola, root_dir, resources_dir):
-#
-#     dymola.loadClass(root_dir / 'fmpy' / 'modelica' / 'FMI' / 'package.mo')
-#
-#     dymola.loadClass(resources_dir / 'FMITest' / 'package.mo')
-#
-#     for model in ['BouncingBall', 'Feedthrough', 'Integrator', 'Stair', 'ThreeSprings', 'Types']:
-#         if not (resources_dir / f'{model}.fmu').exists():
-#             dymola.translateModelFMU(f'FMITest.Models.{model}', modelName=model, fmiVersion='2', fmiType='all')
-#             fmu_path = Path(dymola.getWorkingDirectory()) / f'{model}.fmu'
-#             move(fmu_path, resources_dir)
-#
-#     for interface_type, suffix in [('Co-Simulation', 'CS'), ('Model Exchange', 'ME')]:
-#         for model in ['BouncingBall', 'Feedthrough', 'Integrator', 'Stair', 'ThreeSprings', 'Types']:
-#             import_fmu_to_modelica(
-#                 fmu_path=resources_dir / f'{model}.fmu',
-#                 model_path=resources_dir / 'FMITest' / f'{model}_{suffix}.mo',
-#                 interface_type=interface_type,
-#             )
-#
-#     dymola.loadClass(resources_dir / 'FMITest' / 'package.mo')
-#
-#     for suffix in ['CS', 'ME']:
-#         for model in ['BouncingBall', 'Feedthrough', 'Integrator', 'Stair', 'ThreeSprings', 'Types']:
-#             dymola.simulate(f'FMITest.{model}_{suffix}')
-#
+def pymola_available():
+    try:
+        import pymola
+        return True
+    except:
+        return False
+
 
 def test_import_fmu_to_modelica(root_dir, reference_fmus_dist_dir):
 
@@ -52,3 +24,43 @@ def test_import_fmu_to_modelica(root_dir, reference_fmus_dist_dir):
                     model_path=root_dir / 'fmpy' / 'modelica' / 'FMI' / 'Examples' / f'FMI{fmi_version}' / interface_type / f'{model}.mo',
                     interface_type=interface_type,
                 )
+
+
+@pytest.mark.skipif(not pymola_available(), reason="Pymola was not found")
+def test_run_examples_in_dymola(dymola, root_dir, resources_dir):
+
+    dymola.loadClass(root_dir / 'fmpy' / 'modelica' / 'FMI' / 'package.mo')
+
+    for fmi_version in [2, 3]:
+        for interface_type in ['CoSimulation', 'ModelExchange']:
+            for model, start_values, stop_time in [
+                ('BouncingBall', {'e': 0.8}, None),
+                ('Stair', {}, 8),
+                ('Resource', {}, None),
+                ('VanDerPol', {'mu': 2.5}, None),
+                # ('Feedthrough', {}, None),
+            ]:
+
+                filename = str(root_dir / 'fmpy' / 'modelica' / 'FMI' / 'Resources' / 'FMUs' / model)
+
+                model_description = read_model_description(filename)
+
+                outputs = [v.name for v in model_description.modelVariables if v.causality == 'output']
+
+                reference = simulate_fmu(filename=filename, start_values=start_values, output=outputs, stop_time=stop_time, fmi_type=interface_type)
+
+                intial_values = dict(map(lambda i: (f"'{i[0]}'", i[1]), start_values.items()))
+
+                result = dymola.simulate(
+                    model=f'FMI.Examples.FMI{fmi_version}.{interface_type}.{model}',
+                    initialValues=intial_values,
+                    stopTime=stop_time
+                )
+
+                for name in outputs:
+                    _, _, _, i_out = validate_signal(result['Time'], result[f"'{name}'"], reference['time'],  reference[name])
+                    assert not i_out.any()
+
+                for name, expected in intial_values.items():
+                    actual = result[name]
+                    assert np.allclose(actual, expected)

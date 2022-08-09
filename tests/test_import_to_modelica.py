98ec68f1c641d72afab1be17cb3d4fd7f8e22d57
Torsten Sommer <torsten.sommer@3ds.com>
2022-08-09 11:10:37 +0200

    Fix test

--- tests/test_import_to_modelica.py
+++ tests/test_import_to_modelica.py
@@ -1,14 +1,11 @@
 import os
-from pathlib import Path
 
 import numpy as np
 import pytest
-from OMPython import ModelicaSystem
 
 from fmpy import simulate_fmu, read_model_description
 from fmpy.modelica import generate_examples
-from fmpy.util import validate_signal, plot_result
-from pymola import Dsres
+from fmpy.util import validate_signal
 
 
 def pymola_available():
@@ -34,47 +31,48 @@ def test_import_fmu_to_modelica(root_dir, reference_fmus_dist_dir):
     generate_examples()
 
 
-# @pytest.mark.skipif(not om_available(), reason="OpenModelica was not found")
-# def test_run_examples_in_om(reference_fmus_dist_dir):
-#
-#     import os
-#     from OMPython import OMCSessionZMQ
-#
-#     os.environ['OPENMODELICAHOME'] = r'E:\OpenModelica'
-#
-#     omc = OMCSessionZMQ()
-#
-#     assert omc.sendExpression('loadFile("E:/Development/FMPy/fmpy/modelica/FMI/package.mo")')
-#
-#     for fmi_version in [2, 3]:
-#         for interface_type in ['CoSimulation', 'ModelExchange']:
-#             for model, start_values, stop_time in [
-#                 ('BouncingBall', {'e': 0.8}, 3),
-#                 ('Dahlquist', {}, 10),
-#                 ('Stair', {}, 8),
-#                 ('Resource', {}, 1),
-#                 ('VanDerPol', {'mu': 2.5}, 20),
-#                 # ('Feedthrough', {}, None),
-#             ]:
-#
-#                 info = omc.sendExpression(f'simulate(FMI.Examples.FMI{fmi_version}.{interface_type}.{model}, stopTime={stop_time})')
-#
-#                 result = Dsres(info['resultFile'])
-#
-#                 filename = str(reference_fmus_dist_dir / f'{fmi_version}.0' / f'{model}.fmu')
-#
-#                 model_description = read_model_description(filename)
-#
-#                 outputs = [v.name for v in model_description.modelVariables if v.causality == 'output']
-#
-#                 reference = simulate_fmu(filename=filename,
-#                                          # start_values=start_values,
-#                                          output=outputs, stop_time=stop_time,
-#                                          fmi_type=interface_type)
-#
-#                 for name in outputs:
-#                     _, _, _, i_out = validate_signal(result['time'], result[f"'{name}'"], reference['time'],  reference[name])
-#                     assert not i_out.any()
+@pytest.mark.skipif(not om_available() or not pymola_available(), reason="OpenModelica and/or Pymola was not found")
+def test_run_examples_in_om(reference_fmus_dist_dir):
+
+    import os
+    from OMPython import OMCSessionZMQ
+    from pymola import Dsres
+
+    os.environ['OPENMODELICAHOME'] = r'E:\OpenModelica'
+
+    omc = OMCSessionZMQ()
+
+    assert omc.sendExpression('loadFile("E:/Development/FMPy/fmpy/modelica/FMI/package.mo")')
+
+    for fmi_version in [2, 3]:
+        for interface_type in ['CoSimulation', 'ModelExchange']:
+            for model, start_values, stop_time in [
+                ('BouncingBall', {'e': 0.8}, 3),
+                ('Dahlquist', {}, 10),
+                ('Stair', {}, 8),
+                ('Resource', {}, 1),
+                ('VanDerPol', {'mu': 2.5}, 20),
+                # ('Feedthrough', {}, None),
+            ]:
+
+                info = omc.sendExpression(f'simulate(FMI.Examples.FMI{fmi_version}.{interface_type}.{model}, stopTime={stop_time})')
+
+                result = Dsres(info['resultFile'])
+
+                filename = str(reference_fmus_dist_dir / f'{fmi_version}.0' / f'{model}.fmu')
+
+                model_description = read_model_description(filename)
+
+                outputs = [v.name for v in model_description.modelVariables if v.causality == 'output']
+
+                reference = simulate_fmu(filename=filename,
+                                         # start_values=start_values,
+                                         output=outputs, stop_time=stop_time,
+                                         fmi_type=interface_type)
+
+                for name in outputs:
+                    _, _, _, i_out = validate_signal(result['time'], result[f"'{name}'"], reference['time'],  reference[name])
+                    assert not i_out.any()
 
 
 @pytest.mark.skipif(not pymola_available(), reason="Pymola was not found")

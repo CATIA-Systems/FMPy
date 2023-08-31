4f6050296d045e023c3a11112fd09f91765626ce
Nicolas LAURENT <nicolas.laurent@renault.com>
2023-08-31 22:07:05 +0200

    Fix compilation script

--- build_remoting.py
+++ build_remoting.py
@@ -11,62 +11,42 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 if os.name == 'nt':
 
-    for bitness, architecture in [('win32', 'Win32'), ('win64', 'x64')]:
+    for directory, architecture in [('win32', 'Win32'), ('win64', 'x64')]:
 
         # clean up
-        shutil.rmtree(os.path.join(basedir, 'remoting', bitness), ignore_errors=True)
+        shutil.rmtree(os.path.join(basedir, 'remoting', directory), ignore_errors=True)
 
-        print(f"Building remoting binaries for {bitness}...")
+        print(f"Building remoting binaries for {directory}...")
 
         check_call(args=[
             'cmake',
             '-G', 'Visual Studio 17 2022',
             '-A', architecture,
-            '-B', 'remoting/' + bitness,
+            '-B', 'remoting/' + directory,
             'remoting'
         ])
 
-        check_call(['cmake', '--build', 'remoting/' + bitness, '--config', config])
-
-elif os.name == 'linux':
-    # clean up
-    shutil.rmtree(os.path.join(basedir, 'remoting', 'linux64'), ignore_errors=True)
-
-    print("Building remoting binaries...")
-
-    check_call(args=[
-        'cmake',
-        '-G', 'Unix Makefiles',
-        '-B', 'remoting/linux64',
-        'remoting'
-    ])
-
-    check_call(['cmake', '--build', 'remoting/linux64', '--config', config])
-
-    shutil.rmtree(os.path.join(basedir, 'remoting', 'linux32'), ignore_errors=True)
-    print("Building remoting binaries...")
-
-    check_call(args=[
-        'cmake',
-        '-G', 'Unix Makefiles',
-        '-B', 'remoting/linux32',
-        '-DBUILD_32=ON',
-        'remoting'
-    ])
-
-    check_call(['cmake', '--build', 'remoting/linux32', '--config', config])
+        check_call(['cmake', '--build', 'remoting/' + directory, '--config', config])
 
 else:
-    # clean up
-    shutil.rmtree(os.path.join(basedir, 'remoting', 'darwin64'), ignore_errors=True)
+    import platform
 
-    print("Building remoting binaries...")
+    if platform.system() == 'Linux':
+        compilation = [('linux64', 'OFF'), ('linux32', 'ON')]
+    else:
+        compilation = [('darwin64', 'OFF')]
 
-    check_call(args=[
-        'cmake',
-        '-G', 'Unix Makefiles',
-        '-B', 'remoting/darwin64',
-        'remoting'
-    ])
+    for directory, build_32 in compilation:
+        # clean up
+        shutil.rmtree(os.path.join(basedir, 'remoting', directory), ignore_errors=True)
+        print("Building remoting binaries...")
 
-    check_call(['cmake', '--build', 'remoting/darwin64', '--config', config])
+        check_call(args=[
+            'cmake',
+            '-G', 'Unix Makefiles',
+            '-B', f'remoting/{directory}',
+            f'-DBUILD_32={build_32}',
+            'remoting'
+        ])
+
+        check_call(['cmake', '--build', f'remoting/{directory}', '--config', config])

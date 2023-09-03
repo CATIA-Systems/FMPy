89f5857ce3b72f125dcaa3e2ad3f2c30bdd7d510
Nicolas LAURENT <nl@haplo.info>
2023-09-03 10:23:50 +0200

    Try to run server_sm direclty from python

--- fmpy/util.py
+++ fmpy/util.py
@@ -1034,8 +1034,12 @@ def add_remoting(filename, host_platform, remote_platform):
     server_dst_file = join(tempdir, 'binaries', remote_platform, f'server_{method}{ex_ext[remote_platform]}')
     copyfile(src=join(current_dir, 'remoting', remote_platform, f'server_{method}{ex_ext[remote_platform]}'),
              dst=server_dst_file)
-    os.chmod(server_dst_file, 0o755)
     
+    os.chmod(server_dst_file, 0o755)
+    import subprocess
+    result = subprocess.run(server_dst_file)
+    result.check_returncode()
+
     licenses_dir = join(tempdir, 'documentation', 'licenses')
     os.makedirs(licenses_dir, exist_ok=True)
     copyfile(license, join(tempdir, 'documentation', 'licenses', 'fmpy-remoting-binaries.txt'))

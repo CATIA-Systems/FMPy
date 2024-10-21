a0f2517ee9d4711a22593ac44789050938ba8b0c
Torsten Sommer <torsten.sommer@3ds.com>
2024-10-21 11:20:18 +0200

    Fix path to sundials libraries on aarch64-darwin (#702)
    fixes #661

--- fmpy/sundials/libraries.py
+++ fmpy/sundials/libraries.py
@@ -1,11 +1,17 @@
 from ctypes import cdll
-import os
+from pathlib import Path
 from fmpy import sharedLibraryExtension, platform_tuple
 
-library_dir, _ = os.path.split(__file__)
+
+library_dir = Path(__file__).parent
+
+if platform_tuple == 'aarch64_darwin':
+    library_dir = library_dir / 'x86_64-darwin'
+else:
+    library_dir = library_dir / platform_tuple
 
 # load SUNDIALS shared libraries
-sundials_nvecserial     = cdll.LoadLibrary(os.path.join(library_dir, platform_tuple, 'sundials_nvecserial'     + sharedLibraryExtension))
-sundials_sunmatrixdense = cdll.LoadLibrary(os.path.join(library_dir, platform_tuple, 'sundials_sunmatrixdense' + sharedLibraryExtension))
-sundials_sunlinsoldense = cdll.LoadLibrary(os.path.join(library_dir, platform_tuple, 'sundials_sunlinsoldense' + sharedLibraryExtension))
-sundials_cvode          = cdll.LoadLibrary(os.path.join(library_dir, platform_tuple, 'sundials_cvode'          + sharedLibraryExtension))
+sundials_nvecserial     = cdll.LoadLibrary(str(library_dir / f'sundials_nvecserial{sharedLibraryExtension}'))
+sundials_sunmatrixdense = cdll.LoadLibrary(str(library_dir / f'sundials_sunmatrixdense{sharedLibraryExtension}'))
+sundials_sunlinsoldense = cdll.LoadLibrary(str(library_dir / f'sundials_sunlinsoldense{sharedLibraryExtension}'))
+sundials_cvode          = cdll.LoadLibrary(str(library_dir / f'sundials_cvode{sharedLibraryExtension}'))

31b3f01961176824e543e295acdb32909f73ac58
Torsten Sommer <torsten.sommer@3ds.com>
2024-12-10 10:30:55 +0100

    Update test_extracted_fmu.py

--- tests/test_extracted_fmu.py
+++ tests/test_extracted_fmu.py
@@ -1,27 +1,22 @@
-import unittest
-
 import shutil
-
 from fmpy import extract, read_model_description, simulate_fmu
 from fmpy.util import download_test_file
 
 
-class ExtractedFMUTest(unittest.TestCase):
+def test_extracted_fmu():
+    """ Simulate an extracted FMU """
 
-    def test_extracted_fmu(self):
-        """ Simulate an extracted FMU """
+    download_test_file('2.0', 'cs', 'MapleSim', '2016.2', 'CoupledClutches', 'CoupledClutches.fmu')
 
-        download_test_file('2.0', 'cs', 'MapleSim', '2016.2', 'CoupledClutches', 'CoupledClutches.fmu')
+    # extract the FMU
+    tempdir = extract('CoupledClutches.fmu')
 
-        # extract the FMU
-        tempdir = extract('CoupledClutches.fmu')
+    # load the model description before the simulation
+    model_description = read_model_description(tempdir)
 
-        # load the model description before the simulation
-        model_description = read_model_description(tempdir)
+    result = simulate_fmu(tempdir, model_description=model_description)
 
-        result = simulate_fmu(tempdir, model_description=model_description)
+    assert result is not None
 
-        self.assertIsNotNone(result)
-
-        # clean up
-        shutil.rmtree(tempdir)
+    # clean up
+    shutil.rmtree(tempdir)

e45735e7968c0fb38aa46654b73fac5f250277ff
Torsten Sommer <torsten.sommer@3ds.com>
2025-04-28 09:49:33 +0200

    Bump version to 0.3.23

--- src/fmpy/__init__.py
+++ src/fmpy/__init__.py
@@ -6,7 +6,7 @@ from platform import machine
 from ctypes import *
 from typing import Union, IO, List
 
-__version__ = '0.3.22'
+__version__ = '0.3.23'
 
 # library to use in plot_result()
 plot_library = 'plotly'  # or 'matplotlib'

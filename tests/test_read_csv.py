ffb30e8ebb203e45956de452ae28f200a3886cf5
Torsten Sommer <torsten.sommer@3ds.com>
2025-05-13 09:44:57 +0200

    Handle array variables in CSV files

--- tests/test_read_csv.py
+++ tests/test_read_csv.py
@@ -2,33 +2,27 @@ import numpy as np
 from fmpy.util import write_csv, read_csv
 
 
-def test_structured_csv():
+def test_write_and_read_csv(work_dir):
 
     cols = [
         ('time', np.float64, None),
-        ('y', np.float64, (3,)),
+        ('y1', np.float64, None),
+        ('y2', np.float64, (3,)),
     ]
 
     rows = [
-        (0.0, (1.0, 2.0, 3.0)),
-        (0.1, (1.1, 2.1, 3.1)),
-        (0.2, (1.2, 2.2, 3.2)),
-        (0.3, (1.3, 2.3, 3.3)),
+        (0.0, 0, (1.0, 2.0, 3.0)),
+        (0.1, 1, (1.1, 2.1, 3.1)),
+        (0.2, 2, (1.2, 2.2, 3.2)),
+        (0.3, 3, (1.3, 2.3, 3.3)),
     ]
 
-    # create a structured array with a 1-d array signal
-    result = np.array(rows, dtype=np.dtype(cols))
+    expected = np.array(rows, dtype=np.dtype(cols))
 
-    # arrays are saved as single columns
-    write_csv('structured.csv', result)
+    filename = work_dir / 'arrays.csv'
 
-    # read as-is (single columns)
-    traj = read_csv('structured.csv')
+    write_csv(filename, expected)
 
-    assert traj.dtype.names == ('time', 'y[1]', 'y[2]', 'y[3]')
+    result = read_csv(filename)
 
-    # read structured (restore arrays)
-    traj = read_csv('structured.csv', structured=True)
-
-    assert traj.dtype.names == ('time', 'y')
-    assert traj['y'].shape == (4, 3)
+    assert np.all(result == expected)

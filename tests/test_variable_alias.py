e01c2a714b8bcfde206078a4bbd43f9d56c2bf80
Torsten Sommer <torsten.sommer@3ds.com>
2025-12-12 11:51:34 +0100

    Add FMI 3.0 alias variables to model variables

--- tests/test_variable_alias.py
+++ tests/test_variable_alias.py
@@ -1,17 +1,8 @@
-from fmpy import read_model_description
+from fmpy import simulate_fmu
+import numpy as np
 
 
 def test_variable_alias(reference_fmus_dist_dir):
-    """ Read a variable with an alias """
-
-    filename = reference_fmus_dist_dir / '3.0' / 'BouncingBall.fmu'
-
-    model_description = read_model_description(filename)
-
-    h = model_description.modelVariables[1]
-
-    h_ft = h.aliases[0]
-
-    assert h_ft.name == 'h_ft'
-    assert h_ft.description == 'Position in feet'
-    assert h_ft.displayUnit == 'ft'
+    filename = reference_fmus_dist_dir / "3.0" / "BouncingBall.fmu"
+    result = simulate_fmu(filename, output=["h", "h_ft"])
+    assert np.all(result["h"] == result["h_ft"])

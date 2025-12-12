18e4b2d32b56771920fd6ee664ff92e9b7188e52
Torsten Sommer <torsten.sommer@3ds.com>
2025-12-12 14:22:12 +0100

    Restore test

--- tests/test_variable_alias.py
+++ tests/test_variable_alias.py
@@ -1,8 +1,22 @@
-from fmpy import simulate_fmu
+from fmpy import simulate_fmu, read_model_description
 import numpy as np
 
 
 def test_variable_alias(reference_fmus_dist_dir):
-    filename = reference_fmus_dist_dir / "3.0" / "BouncingBall.fmu"
+    """ Read a variable with an alias """
+
+    filename = reference_fmus_dist_dir / '3.0' / 'BouncingBall.fmu'
+
+    model_description = read_model_description(filename)
+
+    h = model_description.modelVariables[1]
+
+    h_ft = h.aliases[0]
+
+    assert h_ft.name == 'h_ft'
+    assert h_ft.description == 'Position in feet'
+    assert h_ft.displayUnit == 'ft'
+
     result = simulate_fmu(filename, output=["h", "h_ft"])
+    
     assert np.all(result["h"] == result["h_ft"])

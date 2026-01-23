a602a206c7bd954d64d1877804f482f4994edd12
Torsten Sommer <torsten.sommer@3ds.com>
2026-01-23 08:28:42 +0100

    Build only dynamic libraries

--- native/build_cvode.py
+++ native/build_cvode.py
@@ -1,5 +1,4 @@
-from fmpy import sharedLibraryExtension, platform
-from fmpy import platform_tuple as current_platform_tuple
+from fmpy import sharedLibraryExtension, platform_tuple
 from fmpy.util import download_file
 import tarfile
 import os
@@ -10,17 +9,11 @@ from subprocess import check_call
 configuration = 'Release'
 
 if os.name == 'nt':
-    generators = [
-        ('win32', ['-G', 'Visual Studio 17 2022', '-A', 'Win32'], 'i686-windows'),
-        ('win64', ['-G', 'Visual Studio 17 2022', '-A', 'x64'], 'x86_64-windows')
-    ]
     sl_prefix = ''
     sl_suffix = sharedLibraryExtension
 else:
-    generators = [(platform, ['-G', 'Unix Makefiles'], current_platform_tuple)]
     sl_prefix = 'lib'
     sl_suffix = sharedLibraryExtension
-
 
 # clean up
 shutil.rmtree('sundials-7.5.0', ignore_errors=True)
@@ -33,36 +26,33 @@ filename = download_file(
 with tarfile.open(filename, "r:gz") as tar:
     tar.extractall()
 
-for platform, cmake_options, platform_tuple in generators:
+os.makedirs(f'sundials-7.5.0/{platform_tuple}')
 
-    os.makedirs(f'sundials-7.5.0/{platform}/dynamic')
+check_call([
+    'cmake',
+    '-D', 'BUILD_ARKODE=OFF',
+    '-D', 'BUILD_CVODES=OFF',
+    '-D', 'BUILD_IDA=OFF',
+    '-D', 'BUILD_IDAS=OFF',
+    '-D', 'BUILD_KINSOL=OFF',
+    '-D', 'BUILD_STATIC_LIBS=OFF',
+    '-D', 'EXAMPLES_ENABLE_C=OFF',
+    '-D', f'CMAKE_INSTALL_PREFIX=sundials-7.5.0/{platform_tuple}/install',
+    '-D', 'CMAKE_POLICY_DEFAULT_CMP0091=NEW',
+    '-D', 'CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded',
+    '-D', 'CMAKE_OSX_ARCHITECTURES=arm64;x86_64',
+    '-S', 'sundials-7.5.0',
+    '-B', f'sundials-7.5.0/{platform_tuple}'
+])
 
-    # build CVode as dynamic library
-    check_call([
-        'cmake',
-        '-D', 'BUILD_ARKODE=OFF',
-        '-D', 'BUILD_CVODES=OFF',
-        '-D', 'BUILD_IDA=OFF',
-        '-D', 'BUILD_IDAS=OFF',
-        '-D', 'BUILD_KINSOL=OFF',
-        '-D', 'BUILD_STATIC_LIBS=OFF',
-        '-D', 'EXAMPLES_ENABLE_C=OFF',
-        '-D', f'CMAKE_INSTALL_PREFIX=sundials-7.5.0/{platform}/dynamic/install',
-        '-D', 'CMAKE_POLICY_DEFAULT_CMP0091=NEW',
-        '-D', 'CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded',
-        '-D', 'CMAKE_OSX_ARCHITECTURES=arm64;x86_64',
-        '-S', 'sundials-7.5.0',
-        '-B', f'sundials-7.5.0/{platform}/dynamic'
-   ] + cmake_options)
+check_call(['cmake', '--build', f'sundials-7.5.0/{platform_tuple}', '--target', 'install', '--config', configuration])
 
-    check_call(['cmake', '--build', f'sundials-7.5.0/{platform}/dynamic', '--target', 'install', '--config', configuration])
+sundials_binary_dir = os.path.join('..', 'src', 'fmpy', 'sundials', platform_tuple)
 
-    sundials_binary_dir = os.path.join('..', 'src', 'fmpy', 'sundials', platform_tuple)
+os.makedirs(sundials_binary_dir, exist_ok=True)
 
-    os.makedirs(sundials_binary_dir, exist_ok=True)
-
-    for name in ['sundials_core', 'sundials_cvode', 'sundials_nvecserial', 'sundials_sunlinsoldense', 'sundials_sunmatrixdense']:
-        lib_dir = 'bin' if os.name == 'nt' else 'lib'
-        src = os.path.join('sundials-7.5.0', platform, 'dynamic', 'install', lib_dir, sl_prefix + name + sl_suffix)
-        dst = os.path.join(sundials_binary_dir, name + sl_suffix)
-        shutil.copy(src, dst)
+for name in ['sundials_core', 'sundials_cvode', 'sundials_nvecserial', 'sundials_sunlinsoldense', 'sundials_sunmatrixdense']:
+    lib_dir = 'bin' if os.name == 'nt' else 'lib'
+    src = os.path.join('sundials-7.5.0', platform_tuple, 'install', lib_dir, sl_prefix + name + sl_suffix)
+    dst = os.path.join(sundials_binary_dir, name + sl_suffix)
+    shutil.copy(src, dst)

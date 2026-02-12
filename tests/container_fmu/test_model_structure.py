974cf4f889ecc2aa245c16eed46e61348c60c42c
Torsten Sommer <torsten.sommer@3ds.com>
2026-02-12 12:00:45 +0100

    Remove test_model_structure.py

--- tests/container_fmu/test_model_structure.py
+++
@@ -1,6 +0,0 @@
-from fmpy import read_model_description, simulate_fmu
-
-
-def test_fmi2_container():
-    # read_model_description(r"E:\WS\FMPy\tests\work\modelDescription.xml")
-    simulate_fmu(r"E:\WS\FMPy\tests\work", fmi_call_logger=print)

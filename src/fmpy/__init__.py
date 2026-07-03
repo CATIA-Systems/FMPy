69ff67ad757b27c0241ec3bd05f31f949170d54b
Torsten Sommer <torsten.sommer@3ds.com>
2026-07-03 09:04:41 +0200

    Bump version to 0.3.30 (#881)

--- src/fmpy/__init__.py
+++ src/fmpy/__init__.py
@@ -7,7 +7,7 @@ from platform import machine
 from ctypes import *
 from typing import Union, IO, List, Callable
 
-__version__ = '0.3.29'
+__version__ = '0.3.30'
 
 # library to use in plot_result()
 plot_library = 'plotly'  # deprecated
